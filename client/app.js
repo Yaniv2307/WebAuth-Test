f537ec124841b6746e1dbd3b85f041033e209e9f
yanivga <yanivga@matrix.co.il>
2020-12-23 15:31:55 +0200

    platform authenticator

--- client/app.js
+++ client/app.js
@@ -6,6 +6,10 @@ $(document).ready(() => {
         return;
     }
 
+    if(PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable().then(() => {
+      alert("yes!")
+    }));
+
     $('#create-btn').click(async () => {
         //let res = await getChallenge();
         //console.log(res);
@@ -28,13 +32,13 @@ $(document).ready(() => {
               id: window.location.hostname,
             },
             user: {
-              id: new Uint8Array([117, 61,252, 231, 191, 241]), //Uint8Array.from("UZSL85T9AFC", c => c.charCodeAt(0)),
-              name: "lee@webauthn.guide",
-              displayName: "Lee",
+              id: new Uint8Array([79, 252, 83, 72, 214, 7, 89, 26]), //Uint8Array.from("UZSL85T9AFC", c => c.charCodeAt(0)),
+              name: "johndoe",
+              displayName: "John Doe",
             },
             pubKeyCredParams: [{ alg: -7, type: "public-key" }],
             authenticatorSelection: {
-              authenticatorAttachment: "cross-platform",
+              authenticatorAttachment: "platform",
             },
             timeout: 60000,
             attestation: "direct"
@@ -44,6 +48,7 @@ $(document).ready(() => {
             const credential = await navigator.credentials.create({
               publicKey: publicKeyCredentialCreationOptions
             });
+            console.log(credential);
           } else {
             //Handle sign-in the way you did before.
             alert("oops");
